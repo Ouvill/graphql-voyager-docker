abdb93c5c1bc54e3b1c4e90917d9848a2d718c5a
Ouvill <contact@ouvill.net>
2023-01-25 21:57:40 +0900

    feat: use environment variables

--- index.ts
+++ index.ts
@@ -1,38 +1,59 @@
 import express from "express";
+import { readFile } from "node:fs/promises";
 import { graphqlHTTP } from "express-graphql";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { express as voyagerMiddleware } from "graphql-voyager/middleware/index.js";
 
-const app = express();
-const port = 3000;
-
-const typeDefs = `
-type Warrior {
-  id: ID!
-  name: String!
+async function read_schema(path: string) {
+  try {
+    const fileSchema = await readFile(path, "utf8");
+    return fileSchema;
+  } catch {
+    return "";
+  }
 }
 
-type Query {
-  warriors: [Warrior]
+async function main() {
+  const port = process.env.PORT || 3000;
+  const ENDPOINT_URL = process.env.ENDPOINT_URL;
+  const SCHEMA = process.env.SCHEMA;
+  const schema_path = process.env.SCHEMA_PATH || "/app/schema.graphql";
+
+  const schema = SCHEMA ?? (await read_schema(schema_path));
+
+  if (!schema && !ENDPOINT_URL) {
+    console.error("No schema or endpoint url provided");
+    return;
+  }
+
+  const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  if (schema) {
+    const executableSchema = makeExecutableSchema({
+      typeDefs: schema,
+    });
+    app.use(
+      "/graphql",
+      graphqlHTTP({
+        schema: executableSchema,
+        graphiql: true,
+      })
+    );
+  }
+
+  const endpointUrl = ENDPOINT_URL || "/graphql";
+  app.use("/", voyagerMiddleware({ endpointUrl: endpointUrl }));
+
+  app.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
+  });
 }
-`;
 
-const executableSchema = makeExecutableSchema({
-  typeDefs,
+
+process.on("SIGINT", function () {
+  console.log("\nGracefully shutting down from SIGINT (Ctrl-C)");
+  process.exit(1);
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: executableSchema,
-    graphiql: true,
-  })
-);
-
-app.use("/", voyagerMiddleware({ endpointUrl: "/graphql" }));
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+main();
